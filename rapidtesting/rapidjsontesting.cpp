e28ef119888160058c65383205d92e4f198057f5
Zachary <zdonato@stevens.edu>
2015-06-30 11:11:27 -0400

    Rapid json testing.

--- rapidtesting/rapidjsontesting.cpp
+++ rapidtesting/rapidjsontesting.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdio>
 
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
@@ -10,53 +11,52 @@
 int main() {
     
     // Reading in Json file.
-    std::ifstream file ("testing.json");
+    std::ifstream file ("output.json");
     
-//    if (file.is_open())
-//    {
-//        std::string content( (std::istreambuf_iterator<char>(file) ),
-//                            (std::istreambuf_iterator<char>()    ) );
-//        
-//        rapidjson::Document d;
-//        
-//        d.Parse(content.c_str());
-//        
-//        rapidjson::Value& s = d["key"];
-//        
-//        std::cout << s.GetString() << std::endl;
-//        
-//        file.close();
-//    }
-//    else
-//    {
-//       std::cout << "Unable to open file. \n";
-//    }
+    if (file.is_open())
+    {
+        std::string content( (std::istreambuf_iterator<char>(file) ),
+                            (std::istreambuf_iterator<char>()    ) );
+        
+        rapidjson::Document d;
+        
+        d.Parse(content.c_str());
+        
+        rapidjson::Value& s = d["hello"];
+        
+        std::cout << s.GetString() << std::endl;
+        
+        file.close();
+    }
+    else
+    {
+       std::cout << "Unable to open file. \n";
+    }
+    
     
     // Writing to JSON file.
     
     rapidjson::StringBuffer s;
-    rapidjson::Document d;
-    d.SetObject();
     rapidjson::Writer<rapidjson::StringBuffer> writer(s);
     
     writer.StartObject();
-    writer.String("key1");
-    writer.String("value1");
+    writer.String("hello");
+    writer.String("world");
     writer.String("boolean");
     writer.Bool(true);
+    writer.String("test");
+    std::string testing = "blah";
+    writer.String(testing.c_str());
     writer.EndObject();
-    
-    d.Accept(writer);
     
     std::cout << s.GetString() << std::endl;
     
-    std::string json = s.GetString();
+    std::ofstream out_file;
     
-    std::ofstream new_file;
+    out_file.open ("output.json");
     
-    new_file.open("testing.json");
-    new_file << json;
-    new_file.close();
+    out_file << s.GetString();
+    out_file.close();
     
     return 1;
 }
